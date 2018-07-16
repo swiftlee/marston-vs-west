e253121165a66b0b5d8909e417ee3ca2d59abf7d
Alejandro <asantacoloma24@ufl.edu>
2018-07-16 19:19:23 -0400

    Update load.js

--- States/js/load.js
+++ States/js/load.js
@@ -71,6 +71,10 @@ var loadState={
 				game.load.audio('deathSound', 'https://ufosc.github.io/marston-vs-west/States/Assets/deathSound.wav');
 				game.load.audio('respawnSound', 'https://ufosc.github.io/marston-vs-west/States/Assets/respawnSound.wav');
 				game.load.audio('itemSound', 'https://ufosc.github.io/marston-vs-west/States/Assets/itemSound.wav');
+	
+				game.load.image('blueDamageBox', 'https://ufosc.github.io/marston-vs-west/States/Assets/damageBorder1.png');
+				game.load.image('orangeDamageBox', 'https://ufosc.github.io/marston-vs-west/States/Assets/damageBorder2.png');
+				game.load.image('dust', 'https://ufosc.github.io/marston-vs-west/States/Assets/dust.png');
 	},
 	create: function(){
 		music = game.add.audio('menuMusic');
